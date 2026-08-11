0610bb9dc8438eb4560b1ee5097c5d8f24e56940
frantcc17 <frantcc@hotmail.com>
2026-08-11 12:14:46 +0200

    Delete FRONTEND/main.js

--- FRONTEND/main.js
+++
@@ -1,9 +0,0 @@
-/* ============================================================
-   main.js — Punto de entrada de la aplicación
-   ============================================================ */
-import App from "./App.js";
-import { $ } from "./utils/helpers.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  App($("#app"));
-});
